88a63b401fe58f7fbaa3f24ec5487beef2e95d35
Shirjeel Alam <shirjeel20@gmail.com>
2015-10-27 07:35:37 +0500

    Possible fix for RubyInline

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-ENV['INLINEDIR'] = Rails.root.join('tmp').to_s  # for RubyInline
+ENV['INLINEDIR'] = Rails.root.join('tmp', 'cache').to_s  # for RubyInline
 
 # Initialize the Rails application.
 EmoticodeRails::Application.initialize!
