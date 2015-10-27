d2fad2ee18f48e052191487e67bd64e3c0439b3b
Shirjeel Alam <shirjeel20@gmail.com>
2015-10-27 09:56:50 +0500

    Fix for deployment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   facebook = Rails.application.config.secrets['Facebook']
   github = Rails.application.config.secrets['Github']
 
-  provider :facebook, facebook['api_key'], facebook['api_secret']
+  # provider :facebook, facebook['api_key'], facebook['api_secret']
   provider :github, github['client_id'], github['client_secret'], scope: 'user:email'
 end
