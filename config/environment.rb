61e1eeff6c75799858c88c8afbd4b9c4af976b7f
Shirjeel Alam <shirjeel20@gmail.com>
2015-10-31 07:09:41 -0700

    Correcting typo

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,5 @@ require File.expand_path('../application', __FILE__)
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
-ENV['INLINEDIR'] = Rails.root.join('tmp', '.ruby_inline').to_s  # for RubyInline
-
 # Initialize the Rails application.
 EmoticodeRails::Application.initialize!
