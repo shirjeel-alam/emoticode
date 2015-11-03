dfbd630d98fdc0932901660256e86f3051a85110
Shirjeel Alam <shirjeel20@gmail.com>
2015-11-04 02:39:53 +0500

    Remove debugging code from follows_controller

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,47 +2,35 @@ class FollowsController < ApplicationController
   before_filter :authenticate!
 
   def follow
-    begin
-      respond_to do |format|
-        format.html { redirect_to '/' }
-        format.js {
-          @follow = Follow.new({
-            :owner => @current_user,
-            :follow_type => params[:type],
-            :follow_id => params[:id]
-          })
-          @follow.save!
+    respond_to do |format|
+      format.html { redirect_to '/' }
+      format.js {
+        @follow = Follow.new({
+          :owner => @current_user,
+          :follow_type => params[:type],
+          :follow_id => params[:id]
+        })
+        @follow.save!
 
-          Event.new_follow( @current_user, params[:type].to_i, params[:id].to_i ) 
+        Event.new_follow( @current_user, params[:type].to_i, params[:id].to_i ) 
 
-          if params[:type].to_i == Follow::TYPES[:user]
-            followed = User.find( params[:id] )
-            if followed.profile.follow_mail?
-              FollowMailer.follow( followed, @current_user ).deliver     
-            end
+        if params[:type].to_i == Follow::TYPES[:user]
+          followed = User.find( params[:id] )
+          if followed.profile.follow_mail?
+            FollowMailer.follow( followed, @current_user ).deliver     
           end
-        }
-      end
-    rescue Exception => e
-      logger = Logger.new(Rails.root.join('log', 'emoticode.log').to_s)
-      logger.info e
-      redirect_to '/' and return
+        end
+      }
     end
   end
 
   def unfollow
-    begin
-      respond_to do |format|
-        format.html { redirect_to '/' }
-        format.js { 
-          @follow = Follow.where( :owner => @current_user ).where( :follow_type => params[:type] ).where( :follow_id => params[:id] ).first
-          @follow.destroy!
-        } 
-    end
-    rescue Exception => e
-      logger = Logger.new(Rails.root.join('log', 'emoticode.log').to_s)
-      logger.info e
-      redirect_to '/' and return
+    respond_to do |format|
+      format.html { redirect_to '/' }
+      format.js { 
+        @follow = Follow.where( :owner => @current_user ).where( :follow_type => params[:type] ).where( :follow_id => params[:id] ).first
+        @follow.destroy!
+      }
     end
   end
 end
